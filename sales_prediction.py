8e84e3e08b540ea519b1b1c2258ad65e4c23734a
Vijayamalini-M <m.vijayamalini3336@gmail.com>
2026-05-08 21:46:01 +0530

    Added Sales Prediction Project

---
+++ sales_prediction.py
@@ -0,0 +1,27 @@
+﻿import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, r2_score
+
+# Load dataset
+data = pd.read_csv("Advertising.csv")
+
+# Features & Target
+X = data[['TV', 'Radio', 'Newspaper']]
+y = data['Sales']
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Prediction
+y_pred = model.predict(X_test)
+
+# Evaluation
+print("MAE:", mean_absolute_error(y_test, y_pred))
+print("R2 Score:", r2_score(y_test, y_pred))
